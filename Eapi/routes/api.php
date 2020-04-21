c48122e9b03e0e2626f2a32125a801207902f042
Solari <chaker-2@hotmail.com>
2020-04-21 23:03:56 +0100

    Created Model -a

---
+++ Eapi/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('/products','ProductController');
+
+
+Route::group(['prefix'=>'products'],function(){
+
+    Route::apiResource('/{product}/reviews','ReviewController');
+});

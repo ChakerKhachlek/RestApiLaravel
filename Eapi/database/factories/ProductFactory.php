e6ecceb113bfd5e8b1915f69c95096213c587ec8
Solari <chaker-2@hotmail.com>
2020-04-22 02:21:34 +0100

    Seeding part for Product and Review (factories faker ect)

--- Eapi/database/factories/ProductFactory.php
+++ Eapi/database/factories/ProductFactory.php
@@ -8,5 +8,10 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         //
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100,1000),
+        'stock'=>$faker->randomDigit,
+        'discount'=>$faker->numberBetween(2,30)
     ];
 });

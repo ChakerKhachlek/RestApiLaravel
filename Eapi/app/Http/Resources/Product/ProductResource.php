46c285f3f214464f238b0a3336d3c43f4fd3bef4
Solari <chaker-2@hotmail.com>
2020-04-23 03:46:35 +0100

    Modified product details in Resource

--- Eapi/app/Http/Resources/Product/ProductResource.php
+++ Eapi/app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
           'name'  => $this->name,
           'description'=>$this->detail,
           'price'=>$this->price,
-          'stock'=>$this->stock,
-          'discount'=>$this->discount
+          'stock'=>$this->stock == 0 ? 'Out Of Stock' : $this->stock,
+          'discount'=>$this->discount,
+          'totalPrice'=>round((1 - ($this->discount/100)) * $this->price,2),
+          'rating'=> $this->reviews->count() > 0 ? 
+          round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Rating Yet',
+          'href'=>[
+              'reviews'=> route('reviews.index',$this->id)
+          ]
         ];
     }
 }

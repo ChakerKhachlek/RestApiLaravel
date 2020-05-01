03c3387e76e018a0bc26623fb5a276aeae079968
Solari <chaker-2@hotmail.com>
2020-05-01 04:48:02 +0100

    Review Resource & Product Pagination

--- Eapi/app/Http/Controllers/ProductController.php
+++ Eapi/app/Http/Controllers/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
     public function index()
     {
         //
-       return ProductCollection::collection(Product::all());
+       return ProductCollection::collection(Product::paginate(6));
     }
 
     /**

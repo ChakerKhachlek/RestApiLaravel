84b50932adf1ed4f265fa1538e7ff7a1951c55c4
Solari <chaker-2@hotmail.com>
2020-05-02 01:01:29 +0100

    Handles The Exceptions

--- Eapi/app/Exceptions/Handler.php
+++ Eapi/app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +53,23 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if($request->expectsJson()){
+            
+        if ($exception instanceof ModelNotFoundException){
+            return response()->json([
+                'errors'=>"Product Model not found"
+            ],Response::HTTP_NOT_FOUND);
+
+        }
+       
+
+        if($exception instanceof NotFoundHttpException){
+            return response()->json([
+                'errors'=>"INCORRECT ROUTE"
+            ],Response::HTTP_NOT_FOUND);
+        }
     }
+
+        return parent::render($request, $exception);
+    } 
 }

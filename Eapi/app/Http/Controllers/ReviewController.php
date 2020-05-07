5c7ce0bafa6db3193eb5ade4f218093c2c97404c
Solari <chaker-2@hotmail.com>
2020-05-07 04:19:48 +0100

    Review Created

--- Eapi/app/Http/Controllers/ReviewController.php
+++ Eapi/app/Http/Controllers/ReviewController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Model\Review;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewController extends Controller
 {
@@ -35,9 +37,14 @@ class ReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request,Product $product)
     {
-        //
+        $review=new Review($request->all());
+        $product->reviews()->save($review);
+       return response([
+           'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
+
     }
 
     /**
